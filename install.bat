0c191e6fb1710c8ae6a0d72782e8002cf8d69085
ZbyPq <buckleyahk@outlook.com>
2026-04-22 01:48:06 +0800

    1

---
+++ install.bat
@@ -0,0 +1,5 @@
+REM Run this script to automatically install or reinstall all packages,
+REM as it's performed by the Unity Download Assistant.
+REM Needs to be run with superuser permissions.
+
+"UnitySetup64.exe" -UI=reduced /D=C:\Program Files\Unity 2022.3.62f1
